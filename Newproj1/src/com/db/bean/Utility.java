2c29d2d1b4ad4ef2b90b161ef721d5d9ce939905
rabinarayanb <rabinarayanb0002@gmail.com>
2024-06-09 02:37:56 +0530

    Newproj1 Initial code commit and push

---
+++ Newproj1/src/com/db/bean/Utility.java
@@ -0,0 +1,15 @@
+package com.db.bean;
+
+import java.util.Date;
+
+public class Utility {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is Demo utility class");
+		System.out.println("Today's Date:	"+new Date());
+
+	}
+
+}
