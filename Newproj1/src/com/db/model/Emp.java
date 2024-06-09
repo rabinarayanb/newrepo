db078101d6f02ac26737f9488015658bf86e27fb
rabinarayanb <rabinarayanb0002@gmail.com>
2024-06-10 04:12:06 +0530

    Emp class commit and pushing 10 jun 2025

---
+++ Newproj1/src/com/db/model/Emp.java
@@ -0,0 +1,26 @@
+package com.db.model;
+
+public class Emp {
+	String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public float getSalary() {
+		return salary;
+	}
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+	int id;
+	float salary;
+
+}
