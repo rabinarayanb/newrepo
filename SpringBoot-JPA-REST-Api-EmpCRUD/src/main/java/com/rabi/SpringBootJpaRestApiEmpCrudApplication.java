cfb010ef9410d2d22123421d39186b487393e2e3
rabinarayanb <rabinarayanb0002@gmail.com>
2024-09-27 02:04:24 +0530

    "SpringBootUpaRestApplication.java file commit "

---
+++ SpringBoot-JPA-REST-Api-EmpCRUD/src/main/java/com/rabi/SpringBootJpaRestApiEmpCrudApplication.java
@@ -0,0 +1,16 @@
+package com.rabi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaRestApiEmpCrudApplication {
+
+	public static void main(String[] args) {
+		System.out.println("#######SpringBoot-JPA-REST-Api-EmpCRUD#########");
+		SpringApplication.run(SpringBootJpaRestApiEmpCrudApplication.class, args);
+		
+		System.out.println("==========SpringBoot-JPA-REST-Api-EmpCRUD============");
+	}
+
+}
---
+++ SpringBoot-JPA-REST-Api-EmpCRUD/src/test/java/com/rabi/SpringBootJpaRestApiEmpCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaRestApiEmpCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
